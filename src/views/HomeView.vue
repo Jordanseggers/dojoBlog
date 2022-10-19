a1a5e1ef629441c2e18820692691f779cad2f159
Jordan Eggers <66223278+Jordanseggers@users.noreply.github.com>
2022-10-19 18:47:58 -0400

    using refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    home
+    <p ref="p">My name is {{ name }} and age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    const p = ref(null)
+
+    const name = ref('mario') //refrence object is constant but the value can change
+    const age = ref(30) //the ref makes this a reactive value
+
+    name.value//gets the value of the ref
+
+    const handleClick = () => {
+      name.value = 'luigi'
+      age.value = 35
+    }
+
+    return { name: name, age: age, handleClick, p } //when we return a reactive value, we don't have to call .value in the template
   }
 }
 </script>
